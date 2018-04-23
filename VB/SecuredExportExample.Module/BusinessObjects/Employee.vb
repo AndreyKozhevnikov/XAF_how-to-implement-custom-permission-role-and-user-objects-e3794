f9652df62109402664df5c8f2fe5847cc72eab69
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/SecuredExportExample.Module/BusinessObjects/Employee.vb
@@ -0,0 +1,65 @@
+Imports System.ComponentModel
+
+Imports DevExpress.Xpo
+Imports DevExpress.Data.Filtering
+
+Imports DevExpress.ExpressApp
+Imports DevExpress.Persistent.Base
+Imports DevExpress.Persistent.BaseImpl
+Imports DevExpress.Persistent.BaseImpl.PermissionPolicy
+Imports DevExpress.Persistent.Validation
+Imports DevExpress.ExpressApp.Security
+
+Namespace SecuredExportExample.Module.BusinessObjects
+    <DefaultClassOptions, ImageName("BO_Employee")> _
+    Public Class Employee
+        Inherits PermissionPolicyUser
+
+        Public Sub New(ByVal session As Session)
+            MyBase.New(session)
+        End Sub
+        <Association("Employee-Task")> _
+        Public ReadOnly Property Tasks() As XPCollection(Of Task)
+            Get
+                Return GetCollection(Of Task)("Tasks")
+            End Get
+        End Property
+
+    End Class
+    <DefaultClassOptions, ImageName("BO_Task")> _
+    Public Class Task
+        Inherits BaseObject
+
+        Public Sub New(ByVal session As Session)
+            MyBase.New(session)
+        End Sub
+        Private _subject As String
+        Public Property Subject() As String
+            Get
+                Return _subject
+            End Get
+            Set(ByVal value As String)
+                SetPropertyValue("Subject", _subject, value)
+            End Set
+        End Property
+        Private _dueDate As Date
+        Public Property DueDate() As Date
+            Get
+                Return _dueDate
+            End Get
+            Set(ByVal value As Date)
+                SetPropertyValue("DueDate", _dueDate, value)
+            End Set
+        End Property
+        Private _assignedTo As Employee
+        <Association("Employee-Task")> _
+        Public Property AssignedTo() As Employee
+            Get
+                Return _assignedTo
+            End Get
+            Set(ByVal value As Employee)
+                SetPropertyValue("AssignedTo", _assignedTo, value)
+            End Set
+        End Property
+    End Class
+End Namespace
---
+++ VB/SecuredExportExample.Win.MiddleTierSecurityClient/Program.vb
@@ -0,0 +1,55 @@
+Imports System.Configuration
+
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Security
+Imports DevExpress.ExpressApp.Win
+Imports DevExpress.Persistent.Base
+Imports DevExpress.Persistent.BaseImpl
+Imports SecuredExportExample.Module.SecurityObjects
+Imports DevExpress.ExpressApp.Xpo
+Imports DevExpress.ExpressApp.Security.ClientServer.Wcf
+Imports DevExpress.ExpressApp.Security.ClientServer
+Imports System.ServiceModel
+
+Namespace SecuredExportExample.Win
+    Friend NotInheritable Class Program
+
+        Private Sub New()
+        End Sub
+
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread> _
+        Shared Sub Main()
+            Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            EditModelPermission.AlwaysGranted = System.Diagnostics.Debugger.IsAttached
+            Dim winApplication As New SecuredExportExampleWindowsFormsApplication()
+#If EASYTEST Then
+            DevExpress.ExpressApp.Win.EasyTest.EasyTestRemotingRegistration.Register()
+            Dim serverProcess = New System.Diagnostics.Process()
+            serverProcess.StartInfo.FileName = "..\..\..\SecuredExportExample.MiddleTierSecurityServer\bin\EasyTest\SecuredExportExample.MiddleTierSecurityServer.exe"
+            serverProcess.Start()
+            System.Threading.Thread.Sleep(5000)
+#End If
+            Try
+
+                WcfDataServerHelper.AddKnownType(GetType(ExportPermissionRequest))
+                winApplication.ConnectionString = "http://localhost:1451/DataServer"
+                Dim clientDataServer As New WcfSecuredDataServerClient(WcfDataServerHelper.CreateDefaultBinding(), New EndpointAddress(winApplication.ConnectionString))
+                Dim securityClient As New ServerSecurityClient(clientDataServer, New ClientInfoFactory())
+                securityClient.IsSupportChangePassword = True
+                winApplication.ApplicationName = "SecuredExportExample"
+                winApplication.Security = securityClient
+                AddHandler winApplication.CreateCustomObjectSpaceProvider, Sub(sender As Object, e As CreateCustomObjectSpaceProviderEventArgs) e.ObjectSpaceProvider = New DataServerObjectSpaceProvider(clientDataServer, securityClient)
+                winApplication.Setup()
+                winApplication.Start()
+                clientDataServer.Close()
+            Catch e As Exception
+                winApplication.HandleException(e)
+            End Try
+        End Sub
+
+    End Class
+End Namespace
