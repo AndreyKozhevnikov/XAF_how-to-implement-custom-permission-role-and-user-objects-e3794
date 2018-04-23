840ef47e810c9a475beed145e790db69a8e4e0c3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/SecuredExportExample.Module/Module.vb
@@ -0,0 +1,15 @@
+Imports DevExpress.ExpressApp
+Imports System.Reflection
+Imports DevExpress.ExpressApp.Security
+Imports SecuredExportExample.Module.SecurityObjects
+
+
+Namespace SecuredExportExample.Module
+    Public NotInheritable Partial Class SecuredExportExampleModule
+        Inherits ModuleBase
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
