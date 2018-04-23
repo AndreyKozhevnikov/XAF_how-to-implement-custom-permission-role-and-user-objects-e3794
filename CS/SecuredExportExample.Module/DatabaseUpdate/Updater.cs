a80e1a4582548e1d0310f2765af317c3a6f46275
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SecuredExportExample.Module/DatabaseUpdate/Updater.cs
@@ -0,0 +1,94 @@
+using System;
+
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.Xpo;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp.Security;
+using SecuredExportExample.Module.SecurityObjects;
+using SecuredExportExample.Module.BusinessObjects;
+using DevExpress.ExpressApp.Security.Strategy;
+
+namespace SecuredExportExample.Module.DatabaseUpdate {
+    public class Updater : ModuleUpdater {
+        public Updater(IObjectSpace objectSpace, Version currentDBVersion) : 
+            base(objectSpace, currentDBVersion) { }
+        public override void UpdateDatabaseAfterUpdateSchema() {
+            base.UpdateDatabaseAfterUpdateSchema();
+            ExtendedSecurityRole defaultRole = CreateUserRole();
+            ExtendedSecurityRole administratorRole = CreateAdministratorRole();
+            ExtendedSecurityRole exporterRole = CreateExporterRole();
+            Employee userAdmin = ObjectSpace.FindObject<Employee>(new BinaryOperator("UserName", "Admin"));
+            if (userAdmin == null) {
+                userAdmin = ObjectSpace.CreateObject<Employee>();
+                userAdmin.UserName = "Admin";
+                userAdmin.IsActive = true;
+                userAdmin.SetPassword("");
+                userAdmin.Roles.Add(administratorRole);
+                userAdmin.Save();
+            }
+            Employee userSam = ObjectSpace.FindObject<Employee>(new BinaryOperator("UserName", "Sam"));
+            if (userSam == null) {
+                userSam = ObjectSpace.CreateObject<Employee>();
+                userSam.UserName = "Sam";
+                userSam.IsActive = true;
+                userSam.SetPassword("");
+                userSam.Roles.Add(exporterRole);
+                userSam.Roles.Add(defaultRole);
+                userSam.Save();
+            }
+            Employee userJohn = ObjectSpace.FindObject<Employee>(new BinaryOperator("UserName", "John"));
+            if (userJohn == null) {
+                userJohn = ObjectSpace.CreateObject<Employee>();
+                userJohn.UserName = "John";
+                userJohn.IsActive = true;
+                userJohn.Roles.Add(defaultRole);
+                for (int i = 1; i <= 10; i++) {
+                    string subject = string.Format("Task {0}",i);
+                    Task task = ObjectSpace.FindObject<Task>(new BinaryOperator("Subject", subject));
+                    if (task == null) {
+                        task = ObjectSpace.CreateObject<Task>();
+                        task.Subject = subject;
+                        task.DueDate = DateTime.Today;
+                        task.Save();
+                        userJohn.Tasks.Add(task);
+                    }
+                }
+                userJohn.Save();
+            }
+        }
+        private ExtendedSecurityRole CreateAdministratorRole() {
+            ExtendedSecurityRole administratorRole = ObjectSpace.FindObject<ExtendedSecurityRole>(
+                new BinaryOperator("Name", SecurityStrategyComplex.AdministratorRoleName));
+            if (administratorRole == null) {
+                administratorRole = ObjectSpace.CreateObject<ExtendedSecurityRole>();
+                administratorRole.Name = SecurityStrategyComplex.AdministratorRoleName;
+                administratorRole.IsAdministrative = true;
+            }
+            return administratorRole;
+        }
+        private ExtendedSecurityRole CreateExporterRole() {
+            ExtendedSecurityRole exporterRole = ObjectSpace.FindObject<ExtendedSecurityRole>(
+                new BinaryOperator("Name", "Exporter"));
+            if (exporterRole == null) {
+                exporterRole = ObjectSpace.CreateObject<ExtendedSecurityRole>();
+                exporterRole.Name = "Exporter";
+                exporterRole.CanExport = true;
+            }
+            return exporterRole;
+        }
+        private ExtendedSecurityRole CreateUserRole() {
+            ExtendedSecurityRole userRole = ObjectSpace.FindObject<ExtendedSecurityRole>(
+                new BinaryOperator("Name", "Default"));
+            if (userRole == null) {
+                userRole = ObjectSpace.CreateObject<ExtendedSecurityRole>();
+                userRole.Name = "Default";
+                userRole.SetTypePermissions<Task>(SecurityOperations.FullAccess, SecuritySystemModifier.Allow);
+                userRole.SetTypePermissions<Employee>(SecurityOperations.ReadOnlyAccess, SecuritySystemModifier.Allow);
+                userRole.AddObjectAccessPermission<SecuritySystemUser>(
+                 "[Oid] = CurrentUserId()", SecurityOperations.ReadOnlyAccess);
+            }
+            return userRole;
+        }
+    }
+}
---
+++ VB/SecuredExportExample.Win/Program.vb
@@ -0,0 +1,46 @@
+Imports System.Configuration
+
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Security
+Imports DevExpress.ExpressApp.Win
+Imports DevExpress.Persistent.Base
+Imports DevExpress.Persistent.BaseImpl
+Imports SecuredExportExample.Module.SecurityObjects
+Imports DevExpress.ExpressApp.Xpo
+
+Namespace SecuredExportExample.Win
+	Friend NotInheritable Class Program
+
+		Private Sub New()
+		End Sub
+
+		''' <summary>
+		''' The main entry point for the application.
+		''' </summary>
+		<STAThread> _
+		Shared Sub Main()
+			Application.EnableVisualStyles()
+			Application.SetCompatibleTextRenderingDefault(False)
+			EditModelPermission.AlwaysGranted = System.Diagnostics.Debugger.IsAttached
+			Dim winApplication As New SecuredExportExampleWindowsFormsApplication()
+#If EASYTEST Then
+			DevExpress.ExpressApp.Win.EasyTest.EasyTestRemotingRegistration.Register()
+#End If
+			Try
+				InMemoryDataStoreProvider.Register()
+				winApplication.ConnectionString = InMemoryDataStoreProvider.ConnectionString
+				AddHandler CType(winApplication.Security, SecurityStrategy).CustomizeRequestProcessors, Function(sender, e) AnonymousMethod1(sender, e)
+				winApplication.Setup()
+				winApplication.Start()
+			Catch e As Exception
+				winApplication.HandleException(e)
+			End Try
+		End Sub
+		
+		Private Shared Function AnonymousMethod1(ByVal sender As Object, ByVal e As CustomizeRequestProcessorsEventArgs) As Object
+			e.Processors.Add(GetType(ExportPermissionRequest), New ExportPermissionRequestProcessor(e.Permissions))
+			Return Nothing
+		End Function
+
+	End Class
+End Namespace
